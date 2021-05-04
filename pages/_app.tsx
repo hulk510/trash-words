2a0b75a68e52028cd2aabc6c97ef838738b2a582
Haruka <dorian.51069@gmail.com>
2021-05-04 17:21:21 +0900

    add: material-ui

---
+++ pages/_app.tsx
@@ -0,0 +1,36 @@
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Head from "next/head";
+import PropTypes from "prop-types";
+import { useEffect } from "react";
+import "../styles/globals.css";
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Everything Garbage</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
